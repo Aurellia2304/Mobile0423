8915f13e7f6a0d51a89b445cde7074ca18a2b456
ayaa <aaurellia23@gmail.com>
2026-04-17 13:36:29 +0700

    Jobsheet 5

--- Pertemuan5/namer_app/lib/main.dart
+++ Pertemuan5/namer_app/lib/main.dart
@@ -160,20 +160,21 @@ class FavoritesPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
-
     if (appState.favorites.isEmpty) {
-      return const Center(child: Text('No favorites yet.'));
+      return Center(child: Text('No favorites yet.'));
     }
-
     return ListView(
       children: [
         Padding(
           padding: const EdgeInsets.all(20),
-          child: Text('You have ${appState.favorites.length} favorites:'),
+          child: Text(
+            'You have '
+            '${appState.favorites.length} favorites:',
+          ),
         ),
         for (var pair in appState.favorites)
           ListTile(
-            leading: const Icon(Icons.favorite),
+            leading: Icon(Icons.favorite),
             title: Text(pair.asLowerCase),
           ),
       ],
